a62429fa1bd9d256eee7e7e5c07584571f15bbda
saif-ullah-0x <itssaifullah0@gmail.com>
2026-08-29 18:30:26 +0500

    Fix build: remove bugged sitemap plugin permanently

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://appliancerepairlakelandfl.com',
   output: 'static',
   integrations: [
-    tailwind(),
-    sitemap()
+    tailwind()
   ]
 });
